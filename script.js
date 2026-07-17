16235577096444d0232ee896c621876ff25def35
toolverseterritory <toolverseterritory@gmail.com>
2026-07-17 23:59:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function comingSoon(tool){
     alert(tool + " feature is under development!");
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      if (!button.hasAttribute("onclick")) {
+        alert("🚀 This tool will be available soon!");
+      }
+    });
+  });
+});
